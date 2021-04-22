0536f770918ad93243c7ae83ccd5d491342b6eb1
Milen <milen@live.unc.edu>
2021-04-22 15:48:19 -0400

    Created autopay

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const app = express();
 const dotenv = require("dotenv");
 const verifyToken = require("./routes/validate-token");
-const cors = require('cors');
+const cors = require("cors");
+const Property = require("./models/Property");
+const User = require("./models/User");
 app.use(cors());
-
 
 // Import all routes
 const authenticationRoute = require("./routes/authentication");
@@ -14,21 +15,20 @@ const userGettersRoute = require("./routes/userGetters");
 
 dotenv.config();
 
-
 // Inform the console when the server starts
-app.listen((process.env.PORT || 5000), () => {
-    console.log("Server has started.");
-})
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server has started.");
+});
 
 // Connect to MongoDB
 mongoose.connect(
-    "mongodb+srv://temp60:420@milencluster.zmag3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-    () => console.log("Successfully Connected to MongoDB.")
-  );
+  "mongodb+srv://temp60:420@milencluster.zmag3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log("Successfully Connected to MongoDB.")
+);
 
 // Read request bodies as JSON
 app.use(express.json());
@@ -38,3 +38,54 @@ app.use("/api/property", verifyToken, manageRoute);
 app.use("/api/user/", verifyToken, userGettersRoute);
 
 //TODO Add Leaderboard
+
+// Every minute, update user balance
+setInterval(async () => {
+  console.log("Starting to pay users...");
+  let allUsers = await User.find({});
+
+  // Ignore users who don't own anything
+  allUsers = allUsers.filter((e) => {
+    return e.properties.length > 0;
+  });
+
+  // Get all propertes
+  let allProperties = await Property.find({});
+
+  // Iterate over each user
+  allUsers.forEach((u) => {
+    // Iterate over each property they own
+    const startBalance = u.balance;
+    u.properties.forEach((currProperty) => {
+      // Find the property with this ID
+      let targetProperty = allProperties.find((c) => {
+        return String(c._id) == String(currProperty);
+      });
+
+      if (!targetProperty) {
+        console.log("ERROR");
+        return;
+      }
+
+      if (targetProperty.ownerEmailT1 == u.email) {
+        u.balance += targetProperty.hourlyIncome ** 1;
+      }
+      if (targetProperty.ownerEmailT2 == u.email) {
+        u.balance += targetProperty.hourlyIncome ** 2;
+      }
+      if (targetProperty.ownerEmailT3 == u.email) {
+        u.balance += targetProperty.hourlyIncome ** 3;
+      }
+      if (targetProperty.ownerEmailT4 == u.email) {
+        u.balance += targetProperty.hourlyIncome ** 4;
+      }
+      if (targetProperty.ownerEmailT5 == u.email) {
+        u.balance += targetProperty.hourlyIncome ** 5;
+      }
+    });
+
+    // Save changes, move onto next user
+    console.log(`Credited ${u.email} with ${u.balance - startBalance}`);
+    u.save();
+  });
+}, 5*60*1000);
